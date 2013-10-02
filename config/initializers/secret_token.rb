b78b0624cc3149e1d71e36bbb73383efda6aa9da
William Eddy <willieddy@gmail.com>
2013-10-02 17:41:35 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wolfrailsalpha::Application.config.secret_key_base = 'bc6f159de8cd31d6e64155038c1d365e5bc5998117999bf3f96fd4d55c8d064d46a75fae76f7191ef66a1afb02bb92496d14584216277763149ac421c9c5f467'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wolfrailsalpha::Application.config.session_store :cookie_store, key: '_wolfrailsalpha_session'
